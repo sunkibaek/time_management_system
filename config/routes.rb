550530ba4384b56be0fb9b30d02caf7e3d2f0bb2
Sunki Baek <sunki.baek@gmail.com>
2015-09-10 13:54:07 +0900

    name space routes for api

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'homes#show'
 
-  resource :session, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create]
+  namespace :api do
+    namespace :v1 do
+      resource :session, only: [:new, :create, :destroy]
+      resources :users, only: [:new, :create]
+    end
+  end
 end
