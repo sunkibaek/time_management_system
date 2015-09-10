953c4e8cfe1942a855bd8be0fed8cc4437ffe1b6
Sunki Baek <sunki.baek@gmail.com>
2015-09-10 09:17:07 +0900

    make sign up form togglable by clicking sign up button

--- spec/features/user_creates_and_logs_in_spec.rb
+++ spec/features/user_creates_and_logs_in_spec.rb
@@ -1,21 +1,20 @@
 require 'rails_helper'
 
 RSpec.feature "UserCreatesAndLogsIn", type: :feature do
-  scenario 'creates an account' do
+  scenario 'creates an account', js: true do
     visit '/'
 
-    expect(page).to have_no_css 'h2', text: 'Sign Up'
+    expect(page).to have_css 'h2', text: 'Sign Up', visible: false
 
     click_on 'Sign Up'
 
-    expect(page).to have_css 'h2', text: 'Sign Up'
+    expect(page).to have_css 'h2', text: 'Sign Up', visible: true
 
     fill_in 'Name', with: 'Test User'
     fill_in 'Email', with: 'test@example.com'
     fill_in 'Password', with: 'TestPassword'
-    fill_in 'Password Confirm', with: 'TestPassword'
 
-    click_on 'Save'
+    click_on 'Submit'
 
     expect(page).to have_text('User account successfully created.')
     expect(page).to have_text('Test User')
