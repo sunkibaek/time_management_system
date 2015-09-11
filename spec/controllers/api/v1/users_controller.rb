e65f1631c685cf7a1ee3a9fddbc258ca8999f537
Sunki Baek <sunki.baek@gmail.com>
2015-09-12 01:04:48 +0900

    add tests for users#me

--- spec/controllers/api/v1/users_controller.rb
+++ spec/controllers/api/v1/users_controller.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 describe Api::V1::UsersController do
-
   describe '#create' do
     context 'with valid input' do
       subject do
@@ -15,6 +14,7 @@ describe Api::V1::UsersController do
         expect{ subject }.to change(User, :count).by 1
         expect(flash[:notice]).to eq Api::V1::UsersController::MSG[:success]
         expect(response).to render_template('api/v1/users/create')
+        expect(response.status).to eq 200
       end
     end
 
@@ -29,6 +29,30 @@ describe Api::V1::UsersController do
         expect{ subject }.to change(User, :count).by 0
         expect(response.body).to eq(
           { notice: Api::V1::UsersController::MSG[:error] }.to_json)
+        expect(response.status).to eq 400 # bad request
+      end
+    end
+  end
+
+  describe '#me' do
+    context 'when logged in' do
+      it 'returns currently logged in user' do
+        user = create :user
+        sign_in user
+
+        get :me, format: :json
+
+        expect(response.status).to eq 200
+        expect(response.body).to eq({ name: user.name }.to_json)
+      end
+    end
+
+    context 'when not logged in' do
+      it 'returns empty hash' do
+        get :me, format: :json
+
+        expect(response.status).to eq 200
+        expect(response.body).to eq({}.to_json)
       end
     end
   end
