3ef1ca6cbc1de043346afa9ab491bac2e2a88f30
Sunki Baek <sunki.baek@gmail.com>
2015-09-12 00:57:01 +0900

    add log in tests

--- spec/features/user_creates_and_logs_in_spec.rb
+++ spec/features/user_creates_and_logs_in_spec.rb
@@ -19,4 +19,24 @@ RSpec.feature "UserCreatesAndLogsIn", type: :feature do
     expect(page).to have_text('User account successfully created.')
     expect(page).to have_text('Test User')
   end
+
+  scenario 'logs in', js: true do
+    user = create :user
+
+    visit '/'
+
+    expect(page).to have_css 'h2', text: 'Log In', visible: false
+
+    click_on 'Log In'
+
+    expect(page).to have_css 'h2', text: 'Log In', visible: true
+
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password_digest
+
+    click_on 'Submit'
+
+    expect(page).to have_text('Successfully logged in.')
+    expect(page).to have_text(user.name)
+  end
 end
