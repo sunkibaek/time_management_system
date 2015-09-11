637c3700ff1bd2431ee787ac29f6a751ad8dd001
Sunki Baek <sunki.baek@gmail.com>
2015-09-12 00:54:57 +0900

    add routes to fetch current user info

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
       resource :session, only: [:new, :create, :destroy]
-      resources :users, only: [:new, :create]
+      resources :users, only: [:new, :create] do
+        collection do
+          get 'me'
+        end
+      end
     end
   end
 end
