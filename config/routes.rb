02c9645c8352b71d8858ff53829897b7fbb03e92
Sunki Baek <sunki.baek@gmail.com>
2015-09-11 12:04:01 +0900

    limit to json by default for api controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'homes#show'
 
-  namespace :api do
+  namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
       resource :session, only: [:new, :create, :destroy]
       resources :users, only: [:new, :create]
