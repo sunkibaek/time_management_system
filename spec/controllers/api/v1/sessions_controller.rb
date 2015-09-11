3cad7c1cea0956ba866827cfac130cbce00fd47a
Sunki Baek <sunki.baek@gmail.com>
2015-09-12 01:03:32 +0900

    add tests for sessions

---
+++ spec/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe Api::V1::SessionsController do
+  describe '#create' do
+    context 'with valid inputs' do
+      it 'logs the user in' do
+        user = create :user
+
+        post :create,
+          { session: { email: user.email, password: user.password_digest },
+            format: :json }
+
+        expect(response.status).to eq 200
+        expect(response.body).to eq(
+          { notice: Api::V1::SessionsController::MSG[:success] }.to_json)
+      end
+
+      it 'returns error message' do
+        user = create :user
+
+        post :create,
+          { session: { email: '', password: user.password_digest },
+            format: :json }
+
+        expect(response.status).to eq 400
+        expect(response.body).to eq(
+          { notice: Api::V1::SessionsController::MSG[:error] }.to_json)
+      end
+    end
+  end
+end
