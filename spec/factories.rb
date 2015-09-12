976f36f5bcdc6760b056f00769ebf1b7fba70a1b
Sunki Baek <sunki.baek@gmail.com>
2015-09-12 16:35:17 +0900

    add task factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,4 +4,11 @@ FactoryGirl.define do
     sequence(:email) { |n| "test_user#{n}@example.com" }
     password_digest 'testPassword'
   end
+
+  factory :task do
+    description 'Test task description'
+    date '31/12/2014'
+    hour 4
+    user
+  end
 end
