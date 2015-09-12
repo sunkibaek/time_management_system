4b35c6bbdc182ac0c85bfebd6f295b5df7242753
Sunki Baek <sunki.baek@gmail.com>
2015-09-12 16:33:29 +0900

    add tasks#index

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
       resource :session, only: [:create, :destroy]
+      resources :tasks, only: [:index, :create]
       resources :users, only: [:create] do
         collection do
           get 'me'
