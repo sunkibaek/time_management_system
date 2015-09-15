040b4060f0be00c8bd1ec8fc6f377503ba1534fa
Sunki Baek <sunki.baek@gmail.com>
2015-09-15 16:16:18 +0900

    add update and destroy action

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -2,16 +2,28 @@ module Api
   module V1
     class TasksController < ApplicationController
       MSG = { success: 'Task successfully added.',
+              update: 'Task successfully edited.',
+              destroy: 'Task successfully deleted.',
               index: 'Tasks list updated.' }
 
       def index
-        tasks = if signed_in?
-                  current_user.tasks.order_by_date_desc
+        @tasks = if signed_in?
+                   current_user.tasks.order_by_date_desc
+                 else
+                   Task.none
+                 end
+        flash[:notice] = MSG[:index]
+        render status: 200
+      end
+
+      def show
+        @task = if signed_in?
+                  current_user.tasks.find(params[:id])
                 else
                   Task.none
                 end
 
-        render json: { tasks: tasks, notice: MSG[:index] }, status: 200
+        render status: 200
       end
 
       def create
@@ -19,6 +31,26 @@ module Api
 
         if task.save
           render json: { notice: MSG[:success] }, status: 200
+        else
+          render json: {}, status: 400
+        end
+      end
+
+      def update
+        task = current_user.tasks.find(params[:id])
+
+        if task.update(task_params)
+          render json: { notice: MSG[:update] }, status: 200
+        else
+          render json: {}, status: 400
+        end
+      end
+
+      def destroy
+        task = current_user.tasks.find(params[:id])
+
+        if task.destroy
+          render json: { notice: MSG[:destroy] }, status: 200
         else
           render json: {}, status: 400
         end
