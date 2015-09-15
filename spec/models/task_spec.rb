4692796e9b9fb2aac5de2a7bee5e4b9ce019d161
Sunki Baek <sunki.baek@gmail.com>
2015-09-15 15:59:15 +0900

    add tests for total hour

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -9,4 +9,15 @@ describe Task do
       expect(Task.order_by_date_desc).to eq [task_of_newer, task_of_older]
     end
   end
+
+  describe '#total_hour' do
+    it 'sums up total hour for the date by the user' do
+      user = create :user
+
+      create :task, date: '03/03/2015', hour: 5, user: user
+      task = create :task, date: '03/03/2015', hour: 5, user: user
+
+      expect(task.total_hour).to eq 10
+    end
+  end
 end
