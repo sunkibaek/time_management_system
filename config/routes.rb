f542f33319b66a9c29d4514e3df4abed82981e22
Sunki Baek <sunki.baek@gmail.com>
2015-09-15 15:59:59 +0900

    add routings for angular to handle; add routes for task actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   root 'homes#show'
 
+  %w(/log_in /sign_up /tasks /tasks/new /tasks/edit/:id /settings).each do |path|
+    get path, to: 'homes#show'
+  end
+
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
       resource :session, only: [:create, :destroy]
-      resources :tasks, only: [:index, :create]
+      resources :tasks, only: [:index, :create, :update, :show, :destroy]
       resources :users, only: [:create] do
         collection do
           get 'me'
