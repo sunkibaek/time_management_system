84006ecdfd6299a7a09a1c9c634c98b0babd31f1
Sunki Baek <sunki.baek@gmail.com>
2015-09-15 20:48:23 +0900

    add update action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,8 @@ module Api
   module V1
     class UsersController < ApplicationController
       MSG = { success: 'User account successfully created.',
-              error: 'There was an error creating the account.' }
+              update: 'User account successfully updated.',
+              error: 'There was an error processing it.' }
 
       def create
         @user = sign_up(user_params)
@@ -10,6 +11,14 @@ module Api
         if @user.valid?
           sign_in(@user)
           flash[:notice] = MSG[:success]
+        else
+          render json: { notice: MSG[:error] }, status: 400
+        end
+      end
+
+      def update
+        if current_user.update(preferred_working_hour_param)
+          render json: { notice: MSG[:update] }, status: 200
         else
           render json: { notice: MSG[:error] }, status: 400
         end
@@ -30,6 +39,10 @@ module Api
 
       def user_params
         params.require(:user).permit(:name, :email, :password)
+      end
+
+      def preferred_working_hour_param
+        params.require(:user).permit(:preferred_working_hour)
       end
     end
   end
