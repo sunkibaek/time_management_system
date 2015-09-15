33b0ca0dd2ce609fd2dbed002cf4f04113633aaf
Sunki Baek <sunki.baek@gmail.com>
2015-09-15 15:58:50 +0900

    add total hour

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,4 +4,15 @@ class Task < ActiveRecord::Base
   def self.order_by_date_desc
     order(date: :desc)
   end
+
+  # def date=(date_with_time_zone)
+  #   # 2014-01-31T15:00:00.000Z
+  #   converted_date = Date.strptime(date_with_time_zone, '%d-%m-%Y')
+  #
+  #   super(converted_date)
+  # end
+
+  def total_hour
+    self.class.where(user_id: user_id, date: date).sum(:hour)
+  end
 end
