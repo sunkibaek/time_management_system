f91b09c9cbfda0167cf99f1c51efcf11fc077106
Sunki Baek <sunki.baek@gmail.com>
2015-09-15 16:14:24 +0900

    refactor spec; add more specs for managing tasks

--- spec/features/user_manages_tasks_spec.rb
+++ spec/features/user_manages_tasks_spec.rb
@@ -1,44 +1,95 @@
 require 'rails_helper'
 
 feature 'User manages tasks' do
-  scenario 'inputs a task', js: true do
-    user = create :user
+  let!(:user) { create :user }
+
+  before do
     sign_in user
+  end
 
-    visit '/'
-
-    fill_in 'Task', with: 'Sample task description'
-    fill_in 'Date', with: '12/31/2014'
-    fill_in 'Hours', with: '4'
+  scenario 'adds a new task', js: true do
+    visit '/tasks'
 
     click_on 'Add a Task'
+
+    fill_in 'Task', with: 'Sample task description'
+    fill_in 'Date', with: '2014-12-31'
+    fill_in 'Hours', with: '4'
+
+    click_on 'Submit'
 
     expect(page).to have_text('Task successfully added.')
     expect(page).to have_css('table tr td', text: 'Sample task description')
   end
 
-  scenario 'change task list order', js: true do
-    user = create :user
-    sign_in user
-    task_of_older = create :task, date: '9/10/2015', user: user
-    task_of_newer = create :task, date: '9/11/2015', user: user
-    task_of_older_text = "#{task_of_older.description} #{task_of_older.date}" \
-      " #{task_of_older.hour}"
-    task_of_newer_text = "#{task_of_newer.description} #{task_of_newer.date}" \
-      " #{task_of_newer.hour}"
+  scenario 'edits a task', js: true do
+    task = create :task, description: 'Test task desc', user: user
 
-    visit '/'
+    visit '/tasks'
+
+    click_on task.description
+
+    expect(page).to have_field('Task', with: 'Test task desc')
+
+    fill_in 'Task', with: 'Edited task description'
+    fill_in 'Date', with: '2014-12-31'
+    fill_in 'Hours', with: '4'
+
+    click_on 'Submit'
+
+    expect(page).to have_text('Task successfully edited.')
+    expect(page).to have_css('table tr td', text: 'Edited task description')
+  end
+
+  scenario 'deletes a task', js: true do
+    task = create :task, description: 'Test task desc', user: user
+
+    visit "/tasks/edit/#{task.id}"
+
+    click_on 'Delete'
+
+    expect(page).to have_text('Task successfully deleted.')
+  end
+
+  scenario 'change task list order', js: true do
+    task_of_older = create :task, date: '2015-9-10', user: user
+    task_of_newer = create :task, date: '2015-9-11', user: user
+
+    visit '/tasks'
 
     expect(page).to have_css('table tbody tr:first-child',
-                             text: task_of_newer_text)
+                             text: task_text(task_of_newer))
     expect(page).to have_css('table tbody tr:last-child',
-                             text: task_of_older_text)
+                             text: task_text(task_of_older))
 
     find('#date-toggle').click
 
     expect(page).to have_css('table tbody tr:first-child',
-                             text: task_of_older_text)
+                             text: task_text(task_of_older))
     expect(page).to have_css('table tbody tr:last-child',
-                             text: task_of_newer_text)
+                             text: task_text(task_of_newer))
+  end
+
+  scenario 'see the list with overworking days highlighted', js: true do
+    user.preferred_working_hour = 8
+
+    task_of_regular_day = create :task, hour: 3, date: '2015-9-10', user: user
+    task_1_of_over_working_day = create(:task,
+      hour: 5, date: '2015-9-11', user: user)
+    task_2_of_over_working_day = create(:task,
+      hour: 5, date: '2015-9-11', user: user)
+
+    visit '/tasks'
+
+    expect(page).to have_css('table tbody tr.success',
+      text: task_text(task_of_regular_day))
+    expect(page).to have_css('table tbody tr.danger',
+      text: task_text(task_1_of_over_working_day))
+    expect(page).to have_css('table tbody tr.danger',
+      text: task_text(task_2_of_over_working_day))
+  end
+
+  def task_text(task)
+    "#{task.description} #{task.date.strftime('%m-%d-%Y')} #{task.hour}"
   end
 end
