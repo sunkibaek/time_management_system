cf07e7b55301ba83cfd474339fd1f9f145325e27
Sunki Baek <sunki.baek@gmail.com>
2015-09-15 16:01:24 +0900

    change date format; add a new field

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,12 +2,13 @@ FactoryGirl.define do
   factory :user do
     sequence(:name) { |n| "Test User #{n}" }
     sequence(:email) { |n| "test_user#{n}@example.com" }
+    preferred_working_hour 8
     password_digest 'testPassword'
   end
 
   factory :task do
     description 'Test task description'
-    date '31/12/2014'
+    date '2014-12-31'
     hour 4
     user
   end
