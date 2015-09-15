11c82d7e1b269f84354833881153582f851f15f0
Sunki Baek <sunki.baek@gmail.com>
2015-09-15 20:51:04 +0900

    add spec for preferred working hour edit

---
+++ spec/features/user_edits_preferred_working_hour_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+feature 'User edits preferred working hour' do
+  let!(:user) { create :user }
+
+  before do
+    sign_in user
+  end
+
+  scenario 'sees PWH in nav bar and navigates to settings', js: true do
+    visit '/tasks'
+
+    within 'nav' do
+      page.find('li a b', text: user.preferred_working_hour).click
+    end
+
+    expect(page).to have_field('Preferred Working Hour',
+      with: user.preferred_working_hour)
+  end
+
+  scenario 'updates PWH in settings', js: true do
+    visit '/settings'
+
+    expect(page).to have_text 'Preferred working hour: 8'
+
+    fill_in 'Preferred Working Hour', with: '4'
+
+    click_on 'Submit'
+
+    expect(page).to have_text 'Preferred working hour: 4'
+  end
+end
