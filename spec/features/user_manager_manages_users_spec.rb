3c6f6618e4e8f6160d0be9c503ad74da7b8ffaab
Sunki Baek <sunki.baek@gmail.com>
2015-09-17 07:56:31 +0900

    add spec for user manage page

---
+++ spec/features/user_manager_manages_users_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+feature 'User manager manages users' do
+  let!(:regular_user) { create :user, name: 'Regular User' }
+  let!(:user_manager) { create :user, roles: ['regular', 'user_manager'] }
+
+  before do
+    sign_in user_manager
+  end
+
+  scenario 'can see users menu', js: true do
+    visit '/'
+
+    expect(page).to have_css('a', text: 'Users', visible: true)
+
+    sign_out
+
+    sign_in regular_user
+
+    expect(page).to have_css('a', text: 'Users', visible: false)
+  end
+
+  scenario 'can visit user manage page', js: true do
+    visit '/admin/users'
+
+    expect(page).to have_css('h1', text: 'User management')
+
+    sign_out
+
+    sign_in regular_user
+
+    visit '/admin/users'
+
+    expect(page).not_to have_css('h1', text: 'User management')
+  end
+
+  scenario 'changes settings of other users', js: true do
+    visit '/admin/users'
+
+    expect(page).to have_css('table tbody tr td', text: 'Regular User')
+
+    fill_in 'Preferred Working Hour', with: 4
+
+    click_on 'Save'
+
+    expect(page).to have_text('User account successfully updated.')
+  end
+end
