178691a8a3ab0d66e1efbc82486f4966dbed36f9
Sunki Baek <sunki.baek@gmail.com>
2015-09-17 08:54:38 +0900

    add user column validation in backend

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
   has_many :tasks
 
+  validates :name, presence: true
   validates :email, presence: true, uniqueness: true
-  validates :password_digest, presence: true
+  validates :password_digest, presence: true, length: { minimum: 8 }
 
   def user_manager?
     roles.include? 'user_manager'
